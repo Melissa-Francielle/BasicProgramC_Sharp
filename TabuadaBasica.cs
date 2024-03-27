10dde0bb9942a554f3b33288342ed0598fd909dc
Melissa Francielle <129883268+Melissa-Francielle@users.noreply.github.com>
2024-03-27 03:00:03 -0300

    Update TabuadaBasica.cs

--- TabuadaBasica.cs
+++ TabuadaBasica.cs
@@ -6,9 +6,15 @@ namespace HelloWorld{
            int multiplicador, multiplicando, resultado;
 
            for (multiplicador = 1; multiplicador <= 10; multiplicador++){
+                if(multiplicador < 10){
                 Console.WriteLine("┌─────────────┐");
-                Console.WriteLine("│Tabuada do   │" + multiplicador);
+                Console.WriteLine("│Tabuada do " + multiplicador+" │");
                 Console.WriteLine("└─────────────┘");
+                } else if(multiplicador == 10){
+                    Console.WriteLine("┌─────────────┐");
+                    Console.WriteLine("│Tabuada do " + multiplicador+"│");
+                    Console.WriteLine("└─────────────┘");
+                }
                 for(multiplicando = 1; multiplicando <= 10; multiplicando++){
                     resultado = (multiplicador * multiplicando);
                     Console.WriteLine(" "+multiplicador + " x " + multiplicando +" = " + resultado);
